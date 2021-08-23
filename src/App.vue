dc4af859b2b6f82d322ba8c988a5e8396267049f
salmaelhajraoui <salma.hajrawi@gmail.com>
2021-08-23 18:49:50 +0200

    question component and add .eslintrc file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,135 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ctr">
+    <Questions v-if="questionsAnswered < questions.length" :questions="questions" />
+    <Results v-else />
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Questions from './components/Questions.vue'
+import Results from './components/Results.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Questions,
+    Results
+  },
+  data() {
+    return {
+      questionsAnswered: 0,
+      questions: [
+        {
+          q: 'What is 2 + 2?',
+          answers: [
+            {
+              text: '4',
+              is_correct: true
+            },
+            {
+              text: '3',
+              is_correct: false
+            },
+            {
+              text: 'Fish',
+              is_correct: false
+            },
+            {
+              text: '5',
+              is_correct: false
+            }
+          ]
+        },
+        {
+          q: 'How many letters are in the word "Banana"?',
+          answers: [
+            {
+              text: '5',
+              is_correct: false
+            },
+            {
+              text: '7',
+              is_correct: false
+            },
+            {
+              text: '6',
+              is_correct: true
+            },
+            {
+              text: '12',
+              is_correct: false
+            }
+          ]
+        },
+        {
+          q: 'Find the missing letter: C_ke',
+          answers: [
+            {
+              text: 'e',
+              is_correct: false
+            },
+            {
+              text: 'a',
+              is_correct: true
+            },
+            {
+              text: 'i',
+              is_correct: false
+            }
+          ]
+        },
+      ],
+      results: [
+        {
+          min: 0,
+          max: 2,
+          title: 'Try again!',
+          desc: 'Do a little more studying and you may succeed!'
+        },
+        {
+          min: 3,
+          max: 3,
+          title: "Wow, you're a genius!",
+          desc: 'Studying has definitely paid off for you!'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    font-size: 20px;
+    font-family: sans-serif;
+    padding-top: 20px;
+    background: #e6ecf1;
+  }
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: all 0.3s linear;
+  }
+  .fade-leave-active {
+    transition: all 0.3s linear;
+    opacity: 0;
+    position: absolute;
+  }
+  .reset-btn:active, .reset-btn:focus, .reset-btn:hover{
+    border: 0;
+    outline: 0;
+  }
+  .reset-btn {
+    background-color: #ff6372;
+    border: 0;
+    font-size: 22px;
+    color: #fff;
+    padding: 10px 25px;
+    margin: 10px auto;
+    display: block;
+  }
 </style>
---
+++ src/components/Questions.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div class="bar"></div>
+      <div class="status">1 out of 3 questions answered</div>
+    </div>
+    <div
+      class="single-question"
+      v-for="question in questions"
+      :key="question.q"
+    >
+      <div class="question">{{ question.q }}</div>
+      <div
+        class="answers"
+        v-for="answer in question.answers"
+        :key="answer.text"
+      >
+        <div class="answer">{{ answer.text }}1</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['questions']
+}
+</script>
+
+<style scoped>
+  .questions-ctr {
+    position: relative;
+    width: 100%;
+  }
+  .progress {
+    height: 50px;
+    margin-top: 10px;
+    background-color: #ddd;
+    position: relative;
+  }
+  .bar {
+    height: 50px;
+    background-color: #ff6372;
+    transition: all 0.3s linear;
+  }
+  .status {
+    position: absolute;
+    top: 15px;
+    left: 0;
+    text-align: center;
+    color: #fff;
+    width: 100%;
+  }
+  .ctr {
+    margin: 0 auto;
+    max-width: 600px;
+    width: 100%;
+    box-sizing: border-box;
+    position: relative;
+  }
+  .question {
+    width: 100%;
+    padding: 20px;
+    font-size: 32px;
+    font-weight: bold;
+    text-align: center;
+    background-color: #00ca8c;
+    color: #fff;
+    box-sizing: border-box;
+  }
+  .single-question {
+    position: relative;
+    width: 100%;
+  }
+  .answer {
+    border: 1px solid #8e959f;
+    padding: 20px;
+    font-size: 18px;
+    width: 100%;
+    background-color: #fff;
+    transition: 0.2s linear all;
+  }
+  .answer span {
+    display: inline-block;
+    margin-left: 5px;
+    font-size: 0.75em;
+    font-style: italic;
+  }
+  .answer:not(.is-answered) {
+    cursor: pointer;
+  }
+  .answer:not(.is-answered):hover {
+    background-color: #8ce200;
+    border-color: #8ce200;
+    color: #fff;
+  }
+</style>
